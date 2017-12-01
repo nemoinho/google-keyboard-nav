4848dc763552869b8fea47d1fc79ded45a8de77e
Felix Nehrke <felix.nehrke@di-unternehmer.com>
2017-12-01 12:27:24 +0100

    Make it work

--- keyboard-navigation.js
+++ keyboard-navigation.js
@@ -1,20 +1,41 @@
 let altPressed = false;
+let results = [];
+let currentResult = 0;
+window.addEventListener('load', initResults, false);
 window.addEventListener('keydown', toggleAlt, false);
 window.addEventListener('keyup', toggleAlt, false);
-window.addEventListener('keydown', logArrow, false);
+
+function initResults() {
+  results = document.querySelectorAll('#lst-ib, #search :not(#extrares) h3 a, #nav a');
+  currentResult = 0;
+  window.addEventListener('keydown', logArrow, false);
+  document.querySelector('#lst-ib').addEventListener('keydown', logArrow, false);
+}
 
 function toggleAlt(e){
   if (e.key === 'Alt') {
     altPressed = e.type === 'keydown'
+    console.log('altPressed', altPressed)
   }
 }
 
 function logArrow(e) {
   if (altPressed) {
+    e.stopPropagation();
     if (e.key === 'ArrowDown') {
-      console.log('Next');
+      if (currentResult < results.length - 1) {
+        currentResult++;
+      }
+      focusCurrentResult();
     } else  if (e.key === 'ArrowUp') {
-      console.log('Previous');
+      if (currentResult > 0) {
+        currentResult--;
+      }
+      focusCurrentResult();
     }
   }
+}
+
+function focusCurrentResult() {
+  results[currentResult].focus();
 }
